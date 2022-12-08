77295391295e666e7f540958f021a938e361ef69
Galen Elias <gelias@gmail.com>
2022-12-07 21:58:34 -0800

    Day 8, refactored

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -1,44 +1,51 @@
 use itertools::Itertools;
 
+fn calculate_visible(grid: &Vec<Vec<i32>>, visible: &mut Vec<Vec<bool>>, r: usize, c: usize, dr: i32, dc: i32) {
+	let rows = grid.len() as i32;
+	let cols = grid[0].len() as i32;
+
+	let mut max_tree = -1;
+	let mut r = r as i32;
+	let mut c = c as i32;
+
+	while r >= 0 && c >= 0 && r < rows && c < cols {
+		if grid[r as usize][c as usize] > max_tree {
+			visible[r as usize][c as usize] = true;
+			max_tree = grid[r as usize][c as usize];
+		}
+
+		r += dr;
+		c += dc;
+	}
+}
+
+fn viewing_distance(grid: &Vec<Vec<i32>>, r: usize, c: usize, dr: i32, dc: i32) -> usize{
+	let rows = grid.len() as i32;
+	let cols = grid[0].len() as i32;
+
+	let start_height = grid[r][c];
+	let mut r = r as i32 + dr;
+	let mut c = c as i32 + dc;
+	let mut dist = 0;
+
+	while r >= 0 && c >= 0 && r < rows && c < cols {
+		dist += 1;
+		if grid[r as usize][c as usize] >= start_height {
+			break;
+		}
+
+		r += dr;
+		c += dc;
+	}
+	return dist;
+}
+
 fn compute_scenic_score(grid: &Vec<Vec<i32>>, r: usize, c: usize) -> usize {
-	let rows = grid.len();
-	let cols = grid[0].len();
-
-	let mut left_score = c;
-	for i in 1..=c {
-		if grid[r][c - i] >= grid[r][c] {
-			left_score = i;
-			break;
-		}
-	}
-
-	let mut right_score = cols - c - 1;
-	for i in 1..=cols - c - 1 {
-		if grid[r][c + i] >= grid[r][c] {
-			right_score = i;
-			break;
-		}
-	}
-
-	let mut top_score = r;
-	for i in 1..=r {
-		if grid[r - i][c] >= grid[r][c] {
-			top_score = i;
-			break;
-		}
-	}
-
-	let mut bottom_score = rows - r - 1;
-	for i in 1..=rows - r - 1 {
-		if grid[r + i][c] >= grid[r][c] {
-			bottom_score = i;
-			break;
-		}
-	}
-
-	let score = left_score * right_score * top_score * bottom_score;
-
-	return score;
+	let left_score = viewing_distance(grid, r, c, 0, -1);
+	let right_score = viewing_distance(grid, r, c, 0, 1);
+	let top_score = viewing_distance(grid, r, c, -1, 0);
+	let bottom_score = viewing_distance(grid, r, c, 1, 0);
+	return left_score * right_score * top_score * bottom_score;
 }
 
 pub fn solve(inputs: Vec<String>) {
@@ -57,33 +64,12 @@ pub fn solve(inputs: Vec<String>) {
 
 	// Walk from left, right, top, and bottom keeping track of highest tree seen from any direction
 	for r in 0..rows {
-		let mut max_tree_left = -1;
-		let mut max_tree_right = -1;
-		for c in 0..cols {
-			if grid[r][c] > max_tree_left {
-				visible[r][c] = true;
-				max_tree_left = grid[r][c];
-			}
-			if grid[r][cols - c - 1] > max_tree_right {
-				visible[r][cols - c - 1] = true;
-				max_tree_right = grid[r][cols - c - 1];
-			}
-		}
+		calculate_visible(&grid, &mut visible, r, 0, 0, 1);
+		calculate_visible(&grid, &mut visible, r, cols - 1, 0, -1);
 	}
-
 	for c in 0..cols {
-		let mut max_tree_top = -1;
-		let mut max_tree_bottom = -1;
-		for r in 0..rows {
-			if grid[r][c] > max_tree_top {
-				visible[r][c] = true;
-				max_tree_top = grid[r][c];
-			}
-			if grid[rows - r - 1][c] > max_tree_bottom {
-				visible[rows - r - 1][c] = true;
-				max_tree_bottom = grid[rows - r - 1][c];
-			}
-		}
+		calculate_visible(&grid, &mut visible, 0, c, 1, 0);
+		calculate_visible(&grid, &mut visible, rows - 1, c, -1, 0);
 	}
 
 	let part1 = visible.iter().map(|v| v.iter().filter(|b| **b).count()).sum::<usize>();
